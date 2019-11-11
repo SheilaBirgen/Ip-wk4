44a8df25aa9177649522c77e9512ef2a3eedc3b6
SheilaBirgen <jeronobergen@gmail.com>
2019-11-11 08:27:30 +0300

    add a fuction to calculate pizza price

--- js/script.js
+++ js/script.js
@@ -1,13 +1,57 @@
 var price , crust_price, topping_price ;
 let total = 0;
-function Getpizza( name,size,crust,topping, total ){
-  this.name = name;
+//pizza constructor
+function Pizza(size, crust) {
   this.size = size;
   this.crust = crust;
-  this.topping = topping;
-  this.total = total;
+  this.toppings = [];
 }
-$(document).ready(function(){
+//location constructor
+function Location(name, estate) {
+  this.name = name;
+  this.estate = estate;
+}
+//price size
+var sizePrice = {
+  small: 750,
+  medium: 900,
+  large: 1200
+};
+var toppingPrice = [
+  {
+    pepperoni: {
+      small: 50,
+      medium: 100,
+      large: 150
+    },
+     mushroom: {
+      small: 25,
+      medium: 50,
+      large: 75
+    },
+    bacon: {
+      small: 50,
+      medium: 75,
+      large: 100
+    }
+  }
+];
+//crust price
+var crustPrice = {
+  crispy: 300,
+  stuffed: 200,
+  gluten: 100
+};//function calc prize according to size
+function sizeCalcPrice(size) {
+  if (size === "small") {
+    return sizePrice.small * 1;
+  } else if (size === "medium") {
+    return sizePrice.medium * 1;
+  } else {
+    return sizePrice.large * 1;
+  }
+}
+/*$(document).ready(function(){
     $("button.proceed").click(function(event){
         let pname = $(".name option:selected").val();
         let psize = $("#size option:selected").val();
@@ -35,3 +79,19 @@ $(document).ready(function(){
              default:
                console.log("error"); 
            }
+           switch(pcrust){
+            case "0":
+              crust_price = 0;
+            break;
+            case "Crispy":
+              crust_price = 200;
+            break;
+            case "Stuffed":
+              crust_price = 250;
+            break;
+            case "Gluten-free":
+              crust_price = 180;
+            break;
+            default:
+              console.log("No price"); 
+          };
